86a57d840581428dd303f313f0814c6a5426b66e
Andrew Davis <containerize.it@gmail.com>
2025-12-09 14:59:57 -0500

    day three part one.

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,20 @@
 import { fileHandler } from "../helpers/fileHandler.js";
 
-const lines = fileHandler('sample_input.txt', 'utf-8').trim().split('\n');
-let firstMax = lines.map((line) => Math.max(...line));
-console.log(firstMax);
+const lines = fileHandler('input.txt', 'utf-8').split('\n').map(l => l.trim());
+let results = 0;
+
+for (let i =0; i < lines.length; i++) {
+    let cluster = lines[i]
+    let maxAmt = 0
+    for (let j = 0; j < cluster.length; j++) {
+        for (let k = j + 1; k < cluster.length; k++) {
+            const digitOne = cluster[j];
+            const digitTwo = cluster[k];
+            const sum = parseInt(digitOne + digitTwo, 10);
+            if (sum > maxAmt) {
+                    maxAmt = sum;
+                }
+            }
+    }
+    results += maxAmt;
+}
